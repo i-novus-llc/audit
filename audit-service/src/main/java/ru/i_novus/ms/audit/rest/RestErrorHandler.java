e906ac6b9b7ae5e84927601a45a39ad2c0b4a3ed
Danis Zamaliev <daniszamaliev@gmail.com>
2019-07-10 21:16:44 +0300

    remove invalid names and add bindexception message

--- audit-service/src/main/java/ru/i_novus/ms/audit/rest/RestErrorHandler.java
+++ audit-service/src/main/java/ru/i_novus/ms/audit/rest/RestErrorHandler.java
@@ -1,10 +1,13 @@
 package ru.i_novus.ms.audit.rest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Objects;
 
 @ControllerAdvice
 public class RestErrorHandler {
@@ -16,5 +19,14 @@ public class RestErrorHandler {
         ex.printStackTrace();
         String result = ex.getMessage();
         return result;
+    }
+
+    @ExceptionHandler({BindException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public String processValidationError(BindException ex){
+        ex.printStackTrace();
+        String result = Objects.nonNull(ex.getFieldError())? ex.getFieldError().getField() : ex.getMessage();
+        return "Неверный формат данных " + result;
     }
 }
