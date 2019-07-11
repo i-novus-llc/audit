8dc8b68783cd48444f5fa92e4dfc5dcb2e9e5dba
esadykov <esadykov@i-novus.ru>
2019-07-11 12:04:48 +0300

    BF

--- audit-service/src/main/java/ru/i_novus/ms/audit/rest/RestErrorHandler.java
+++ audit-service/src/main/java/ru/i_novus/ms/audit/rest/RestErrorHandler.java
@@ -1,5 +1,7 @@
 package ru.i_novus.ms.audit.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,21 +14,22 @@ import java.util.Objects;
 @ControllerAdvice
 public class RestErrorHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(RestErrorHandler.class);
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    public String processValidationError(Exception ex) {
-        ex.printStackTrace();
-        String result = ex.getMessage();
-        return result;
+    public String processValidationError(Exception e) {
+        logger.error("Internal error", e);
+        return e.getMessage();
     }
 
     @ExceptionHandler({BindException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public String processValidationError(BindException ex){
-        ex.printStackTrace();
-        String result = Objects.nonNull(ex.getFieldError())? ex.getFieldError().getField() : ex.getMessage();
+    public String processValidationError(BindException e){
+        logger.error("Bad Request", e);
+        String result = Objects.nonNull(e.getFieldError())? e.getFieldError().getField() : e.getMessage();
         return "Неверный формат данных " + result;
     }
 }
