e5f56bb0d320b0cc059aa5ffb3b7270f6d926cdb
elibragimova <elibragimova@i-novus.ru>
2019-09-04 10:33:32 +0300

    Revert "[CF] AUDIT-30 Создать сервис для работы с типами событий (audit_event_type). исправления после CR https://jira.i-novus.ru/browse/AUDIT-30"
    This reverts commit 82a95781

--- audit-api/src/main/java/ru/i_novus/ms/audit/service/api/AuditReferenceRest.java
+++ audit-api/src/main/java/ru/i_novus/ms/audit/service/api/AuditReferenceRest.java
@@ -23,7 +23,7 @@ public interface AuditReferenceRest {
 
     @GET
     @Path("/objects")
-    @ApiOperation("Получение объектов")
+    @ApiOperation("Получение типов событий")
     Collection getObjects();
 
     @GET
@@ -33,6 +33,6 @@ public interface AuditReferenceRest {
 
     @GET
     @Path("/sourceApplications")
-    @ApiOperation("Получение наименований систем")
+    @ApiOperation("Получение наименований системы")
     Collection getSourceApplications();
 }
