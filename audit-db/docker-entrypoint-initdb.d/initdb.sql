674fc5e7da5c1e1eb671cc2fb2311fa8ad6d99ba
Alexander Kuznetsov <akuznetcov@i-novus.ru>
2019-09-18 11:07:43 +0300

    [CF] AUDIT-37: в образе исправлена ошибка повторного наката скриптов при старте DB. https://jira.i-novus.ru/browse/AUDIT-37

--- audit-db/docker-entrypoint-initdb.d/initdb.sql
+++ audit-db/docker-entrypoint-initdb.d/initdb.sql
@@ -1,3 +1,3 @@
-SELECT 'CREATE DATABASE audit' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'audit')\gexec
+CREATE DATABASE audit;
 \connect audit
 CREATE EXTENSION IF NOT EXISTS pg_pathman;
