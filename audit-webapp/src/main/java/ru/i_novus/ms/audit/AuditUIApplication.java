0dbdf9ff34d82c8cd242ee7daf3f02ef5812109a
elibragimova <elibragimova@i-novus.ru>
2019-10-10 20:20:54 +0300

    [NF] AUDIT-113 подключение security https://jira.i-novus.ru/browse/AUDIT-113

--- audit-webapp/src/main/java/ru/i_novus/ms/audit/AuditUIApplication.java
+++ audit-webapp/src/main/java/ru/i_novus/ms/audit/AuditUIApplication.java
@@ -1,11 +1,14 @@
 package ru.i_novus.ms.audit;
 
+import net.n2oapp.security.admin.rest.client.AdminRestClientConfiguration;
+import net.n2oapp.security.admin.web.AdminWebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-
+@Import({AdminWebConfiguration.class, AdminRestClientConfiguration.class})
 public class AuditUIApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
