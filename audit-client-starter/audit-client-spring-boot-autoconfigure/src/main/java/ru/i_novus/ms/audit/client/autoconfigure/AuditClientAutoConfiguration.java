62aad4feb1db0bd327b0db920db59f1fa7309063
Гайфутдинов Айдар <agaifutdinov@i-novus.ru>
2019-10-22 11:08:27 +0300

    [CF] AUDIT-108 Реализовать фильтр для аудита web модулей

--- audit-client-starter/audit-client-spring-boot-autoconfigure/src/main/java/ru/i_novus/ms/audit/client/autoconfigure/AuditClientAutoConfiguration.java
+++ audit-client-starter/audit-client-spring-boot-autoconfigure/src/main/java/ru/i_novus/ms/audit/client/autoconfigure/AuditClientAutoConfiguration.java
@@ -1,12 +1,15 @@
 package ru.i_novus.ms.audit.client.autoconfigure;
 
 import net.n2oapp.platform.jaxrs.autoconfigure.EnableJaxRsProxyClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.i_novus.ms.audit.client.AuditClient;
 import ru.i_novus.ms.audit.client.SourceApplicationAccessor;
+import ru.i_novus.ms.audit.client.filter.AuditClientFilter;
 import ru.i_novus.ms.audit.client.impl.StubAuditClientImpl;
 import ru.i_novus.ms.audit.service.api.AuditRest;
 
@@ -16,6 +19,9 @@ public class AuditClientAutoConfiguration {
 
     @Value("${audit.client.sourceApplication:#{null}}")
     private String sourceApplication;
+
+    @Value("${audit.client.filter-url-pattern:#{null}}")
+    private String filterUrlPattern;
 
     @Bean
     @ConditionalOnProperty(prefix = "audit.client", name = "sourceApplication")
@@ -27,5 +33,16 @@ public class AuditClientAutoConfiguration {
     @ConditionalOnProperty(prefix = "audit.client", name = "enabled", havingValue = "false")
     public AuditClient stubAuditClient() {
         return new StubAuditClientImpl();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "audit.client", name = "filter-url-pattern")
+    public FilterRegistrationBean<AuditClientFilter> auditClientFilter(@Autowired AuditClient auditClient) {
+        FilterRegistrationBean<AuditClientFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new AuditClientFilter(auditClient));
+        registrationBean.addUrlPatterns(filterUrlPattern);
+
+        return registrationBean;
     }
 }
---
+++ audit-client/src/main/java/ru/i_novus/ms/audit/client/filter/AuditClientFilter.java
@@ -0,0 +1,104 @@
+package ru.i_novus.ms.audit.client.filter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.cxf.common.util.CollectionUtils;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import ru.i_novus.ms.audit.client.AuditClient;
+import ru.i_novus.ms.audit.client.model.AuditClientRequest;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+public class AuditClientFilter implements Filter {
+
+    private static final String OBJECT_TYPE = "REQUEST";
+    private static final String OBJECT_NAME = "Запрос";
+    private static final Short AUDIT_TYPE = 1;
+
+    private AuditClient auditClient;
+
+    public AuditClientFilter(AuditClient auditClient) {
+        this.auditClient = auditClient;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+        ContentCachingRequestWrapper cachedRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper cachedResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        chain.doFilter(cachedRequest, cachedResponse);
+
+        AuditClientRequest auditClientRequest = new AuditClientRequest();
+        auditClientRequest.setEventType(cachedRequest.getMethod());
+        auditClientRequest.setObjectType(OBJECT_TYPE);
+        auditClientRequest.setObjectId(cachedRequest.getRequestURL() == null ? null : cachedRequest.getRequestURL().toString());
+        auditClientRequest.setObjectName(OBJECT_NAME);
+        auditClientRequest.setSourceWorkstation(cachedRequest.getHeader("User-Agent"));
+        auditClientRequest.setAuditType(AUDIT_TYPE);
+        auditClientRequest.setHostname(cachedRequest.getServerName());
+
+        try {
+            auditClientRequest.setContext(getContextJson(cachedRequest, cachedResponse));
+            auditClient.add(auditClientRequest);
+        } catch (Exception e) {
+            log.error("Error occured while sending audit data", e);
+        } finally {
+            cachedResponse.copyBodyToResponse();
+        }
+    }
+
+    private JsonNode getContextJson(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response)
+            throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode rootNode = mapper.createObjectNode();
+
+        ObjectNode generalNode = mapper.createObjectNode();
+        generalNode.put("Request URL", String.valueOf(request.getRequestURL()));
+        generalNode.put("Request Method", request.getMethod());
+        generalNode.put("Status Code", response.getStatusCode());
+        rootNode.set("General", generalNode);
+
+        List<String> requestHeaders = Collections.list(request.getHeaderNames());
+        if (!CollectionUtils.isEmpty(requestHeaders)) {
+            ObjectNode requestHeadersNode = mapper.createObjectNode();
+            requestHeaders.forEach(header ->
+                    requestHeadersNode.put(header, request.getHeader(header)));
+            rootNode.set("RequestHeaders", requestHeadersNode);
+        }
+
+        byte[] requestPayload = request.getContentAsByteArray();
+        if (requestPayload.length != 0) {
+            JsonNode requestPayloadNode = mapper.readTree(requestPayload);
+            rootNode.set("RequestPayload", requestPayloadNode);
+        }
+
+        Collection<String> responseHeaders = response.getHeaderNames();
+        if (!CollectionUtils.isEmpty(responseHeaders)) {
+            ObjectNode responseHeadersNode = mapper.createObjectNode();
+            responseHeaders.forEach(header ->
+                    responseHeadersNode.put(header, response.getHeader(header)));
+            rootNode.set("ResponseHeaders", responseHeadersNode);
+        }
+
+        byte[] responsePayload = response.getContentAsByteArray();
+        if (responsePayload.length != 0) {
+            JsonNode responsePayloadNode = mapper.readTree(responsePayload);
+            rootNode.set("ResponsePayload", responsePayloadNode);
+        }
+
+        return rootNode;
+    }
+}
