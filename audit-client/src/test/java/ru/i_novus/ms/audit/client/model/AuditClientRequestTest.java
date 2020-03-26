bd2baec23e49ef673b5bacf814ecd61dda1e328d
Alexander Kuznetsov <akuznetcov@i-novus.ru>
2020-03-26 18:19:55 +0300

    [CF] MPSAFEKIDS-418: написание тестов https://jira.i-novus.ru/browse/MPSAFEKIDS-418

--- audit-client/src/test/java/ru/i_novus/ms/audit/client/model/AuditClientRequestTest.java
+++ audit-client/src/test/java/ru/i_novus/ms/audit/client/model/AuditClientRequestTest.java
@@ -14,6 +14,20 @@ import static org.junit.Assert.*;
  **/
 public class AuditClientRequestTest extends BaseAuditClientRequestTest {
 
+    private static final String EVENT_TYPE_NEW = "EventTypeNew";
+    private static final String OBJECT_TYPE_NEW = "ObjectTypeNew";
+    private static final String OBJECT_ID_NEW = "ObjectIdNew";
+    private static final String OBJECT_NAME_NEW = "ObjectNameNew";
+    private static final String USER_ID_NEW = "UserIdNew";
+    private static final String USERNAME_NEW = "usernameNew";
+    private static final String SOURCE_WORKSTATION_NEW = "WorkstationNew";
+    private static final String SOURCE_APPLICATION_NEW = "ApplicationNew";
+    private static final String HOSTNAME_NEW = "HostnameNew";
+    private static final String CONTEXT_NEW = "{\"field\": \"name\", \"value\": \"Значение_New\"}";
+    private static final short AUDIT_TYPE_NEW = 2;
+    private static final String SENDER_NEW = "SenderNew";
+    private static final String RECEIVER_NEW = "ReceiverNew";
+
     private static final LocalDateTime EVENT_DATE = LocalDateTime.now().withNano(0);
     private static final String REQUEST_TO_STRING =
             "AuditClientRequest(eventDate=" + EVENT_DATE + ", auditType=1, context={\"field\": \"name\", \"value\": \"Значение\"}, eventType=AuditClientRequestParam{value='EventType', args=[]}, objectType=AuditClientRequestParam{value='ObjectType', args=[]}, objectId=AuditClientRequestParam{value='ObjectId', args=[]}, objectName=AuditClientRequestParam{value='ObjectName', args=[]}, userId=AuditClientRequestParam{value='UserId', args=[]}, username=AuditClientRequestParam{value='username', args=[]}, sourceWorkstation=AuditClientRequestParam{value='Workstation', args=[]}, sourceApplication=AuditClientRequestParam{value='Application', args=[]}, hostname=AuditClientRequestParam{value='Hostname', args=[]}, sender=AuditClientRequestParam{value='Sender', args=[]}, receiver=AuditClientRequestParam{value='Receiver', args=[]})";
@@ -36,12 +50,81 @@ public class AuditClientRequestTest extends BaseAuditClientRequestTest {
 
     @Test
     public void auditClientRequestEqualsHashTest() {
-        AuditClientRequest request = cloning();
+        AuditClientRequest request = createNewRequest();
+
+        assertEquals(auditClientRequest, auditClientRequest);
         assertEquals(request, auditClientRequest);
+
+        assertNotEquals(null, auditClientRequest);
+
         assertEquals(request.hashCode(), auditClientRequest.hashCode());
     }
 
-    private AuditClientRequest cloning() {
+    @Test
+    public void auditClientRequestNotEqualsTest() {
+        AuditClientRequest request = createNewRequest();
+
+        assertNotEquals(null, auditClientRequest);
+
+        request.setEventDate(LocalDateTime.now().withNano(0).minusMinutes(1));
+        assertNotEquals(request, auditClientRequest);
+
+        request.setEventDate(EVENT_DATE);
+        request.setContext(CONTEXT_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setContext(CONTEXT);
+        request.setAuditType(AUDIT_TYPE_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setAuditType(AUDIT_TYPE);
+        request.setEventType(EVENT_TYPE_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setEventType(EVENT_TYPE);
+        request.setHostname(HOSTNAME_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setHostname(HOSTNAME);
+        request.setObjectId(OBJECT_ID_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setObjectId(OBJECT_ID);
+        request.setObjectName(OBJECT_NAME_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setObjectName(OBJECT_NAME);
+        request.setObjectType(OBJECT_TYPE_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setObjectType(OBJECT_TYPE);
+        request.setReceiver(RECEIVER_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setReceiver(RECEIVER);
+        request.setSender(SENDER_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setSender(SENDER);
+        request.setSourceApplication(SOURCE_APPLICATION_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setSourceApplication(SOURCE_APPLICATION);
+        request.setSourceWorkstation(SOURCE_WORKSTATION_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setSourceWorkstation(SOURCE_WORKSTATION);
+        request.setUserId(USER_ID_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setUserId(USER_ID);
+        request.setUsername(USERNAME_NEW);
+        assertNotEquals(request, auditClientRequest);
+
+        request.setUsername(USERNAME);
+    }
+
+    private AuditClientRequest createNewRequest() {
         AuditClientRequest request = new AuditClientRequest();
         request.setEventType(EVENT_TYPE);
         request.setObjectType(OBJECT_TYPE);
