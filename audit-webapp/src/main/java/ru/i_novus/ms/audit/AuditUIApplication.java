7620028027aaed2904c02dd8ed8fd766aefa1c99
Alexander Kuznetsov <akuznetcov@i-novus.ru>
2020-05-20 10:42:22 +0300

    [CF] AUDIT-111 доработка после мержа мастер ветки https://jira.i-novus.ru/browse/AUDIT-111

--- audit-webapp/src/main/java/ru/i_novus/ms/audit/AuditUIApplication.java
+++ audit-webapp/src/main/java/ru/i_novus/ms/audit/AuditUIApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 import ru.i_novus.ms.audit.web.controller.AuditN2oControllerConfiguration;
+import net.n2oapp.security.admin.rest.client.AdminRestClientConfiguration;
+import net.n2oapp.security.admin.web.AdminWebConfiguration;
 
 @SpringBootApplication
 @Import({AuditN2oControllerConfiguration.class, AdminWebConfiguration.class, AdminRestClientConfiguration.class})
