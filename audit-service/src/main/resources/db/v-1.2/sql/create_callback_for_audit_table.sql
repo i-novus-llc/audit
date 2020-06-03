7c55fdf8437ca35b6501f1784cfb9eb9bc853729
Гайфутдинов Айдар <agaifutdinov@i-novus.ru>
2020-06-03 16:41:35 +0300

    [BF] DOMRFMMS-3572 - UAT. Сервис аудита. relation \"audit_2020_05\" already exists

--- audit-service/src/main/resources/db/v-1.2/sql/create_callback_for_audit_table.sql
+++ audit-service/src/main/resources/db/v-1.2/sql/create_callback_for_audit_table.sql
@@ -6,12 +6,15 @@ DECLARE
  new_relname text;
  field_id text;
 BEGIN
- range_min := (params->>'range_min')::timestamp + interval '1 day';
+ range_min := (params->>'range_min')::timestamp;
  field_id := 'audit_type_id';
  -- generate new name for partition based
  -- on its parent name and lower bound
  new_relname := format('%s_%s', params->>'parent', to_char(range_min, 'YYYY_MM'));
  -- rename partition
+ IF EXISTS(SELECT FROM pg_catalog.pg_tables WHERE schemaname = params->>'partition_schema' AND tablename = new_relname) THEN
+    new_relname := format('%s_%s_%s', params->>'parent', to_char(range_min, 'YYYY'), lpad((to_char(range_min, 'MM')::integer + 1)::text, 2, '0'));
+ END IF;
  EXECUTE format('ALTER TABLE %s.%s RENAME TO %s', params->>'partition_schema', params->>'partition', new_relname);
  --add multilevel partitioning
  PERFORM create_range_partitions(concat(params->>'partition_schema', '.', new_relname), field_id, 1, 1, 10);
