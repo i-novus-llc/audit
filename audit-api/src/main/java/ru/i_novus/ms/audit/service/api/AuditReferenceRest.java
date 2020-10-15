aaac2dc24114d89c0c3dce3037b57ec8d529e4e9
Гайфутдинов Айдар <agaifutdinov@i-novus.ru>
2020-10-15 09:33:30 +0300

    [CF] AUDIT-185 Вынести аудит в opensource https://jira.i-novus.ru/browse/AUDIT-184 Рефакторинг

--- audit-api/src/main/java/ru/i_novus/ms/audit/service/api/AuditReferenceRest.java
+++ audit-api/src/main/java/ru/i_novus/ms/audit/service/api/AuditReferenceRest.java
@@ -1,3 +1,20 @@
+/*
+ *    Copyright 2020 I-Novus
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
 package ru.i_novus.ms.audit.service.api;
 
 import io.swagger.annotations.*;
@@ -8,6 +25,7 @@ import ru.i_novus.ms.audit.criteria.AuditSourceApplicationCriteria;
 import ru.i_novus.ms.audit.model.AuditEventType;
 import ru.i_novus.ms.audit.model.AuditObject;
 import ru.i_novus.ms.audit.model.AuditSourceApplication;
+import ru.i_novus.ms.audit.model.AuditType;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -51,5 +69,5 @@ public interface AuditReferenceRest {
     @GET
     @Path("/auditTypes")
     @ApiOperation("Получение типов журналов")
-    Collection getAuditTypes();
+    Collection<AuditType> getAuditTypes();
 }
